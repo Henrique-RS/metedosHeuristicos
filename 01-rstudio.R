10d19c2be89c9016718ef2f34dcf3b50630e415d
Henrique-RS <89302823+Henrique-RS@users.noreply.github.com>
2021-08-21 10:25:56 -0300

    Aula 01

---
+++ 01-rstudio.R
@@ -0,0 +1,33 @@
+## Configurando Diretório
+getwd()
+
+## Contribuidores
+contributors()
+
+print("Bom Sábado")
+
+## Criar Um Gráfico
+plot(1:25)
+
+## instalar pacotes
+install.packages()
+installed.packages()
+
+## Carregando pacotes
+library(randomForest)
+
+## Descarregando Pacotes
+detach(package:randomForest)
+
+## Ajuda
+help("detach")
+
+?detach
+??detach
+
+install.packages("sos")
+
+library(sos)
+
+findFn("detach")
+
